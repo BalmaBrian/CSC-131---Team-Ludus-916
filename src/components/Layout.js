e2d1bff66382d0521e834bcc691c702c2d40ce86
BalmaBrian <brianalexisalmaguer@gmail.com>
2020-03-08 01:30:04 -0800

    Formatted 🏁!

---
+++ src/components/Layout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Board from "./Board";
+import Card from "./Card";
+import CardCollection from "./CardCollection";
+
+export class Layout extends React.Component {}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./style/index.css";
+import Layout from "./components/Layout";
 
-class Board extends React.Component {
-  render() {
-    return (
-      <div className="movie-board">
-        <CardCollection />
-      </div>
-    );
-  }
-}
-
-class CardCollection extends React.Componet {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-}
-
-class Card extends React.Component {}
-
-// ========================================
-ReactDOM.render(<Board />, document.getElementById("root"));
+const app = document.getElementById("root");
+ReactDOM.render(<Layout />, app);
