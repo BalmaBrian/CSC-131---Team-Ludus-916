48abc170ad7f94d63305962901eece75bd020d04
Brian Almaguer <brianharo2000@gmail.com>
2020-03-09 23:19:06 -0700

    Added error handling.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
-const express = require('express');
+// Main setup
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
-const productRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const productRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
 
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
+app.use(morgan("dev"));
 
+// Routes
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+// Error handling
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+// exporting to server.js
 module.exports = app;
