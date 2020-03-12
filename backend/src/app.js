bac44a0c6bb2f510cf61ed83ce040bda4f6c709d
Brian Almaguer <brianharo2000@gmail.com>
2020-03-11 21:44:36 -0700

    Added middle ware and course error handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,11 +2,30 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
+// middleweare
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// adjusting response with headers
+// Ensure we don't get course error aka security measure
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Acess-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next(); // move on
+});
 
 // Routes
 app.use("/products", productRoutes);
