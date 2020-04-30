e69f0485b4a3727fd7181fe010d53bbaaddb605d
Brian Almaguer <brianharo2000@gmail.com>
2020-04-30 13:53:23 -0700

    Updated backend

--- backend/src/routes/winner.js
+++ backend/src/routes/winner.js
@@ -10,7 +10,7 @@ let db = admin.firestore();
 // with the proper code like 502 or something
 router.get("/", (req, res, next) => {
   res.status(404).json({
-    message: "This is not a available"
+    message: "This is not a available",
   });
 });
 
@@ -18,37 +18,39 @@ router.get("/", (req, res, next) => {
 // with the proper code like 502 or something
 router.post("/", (req, res, next) => {
   res.status(404).json({
-    message: "This is not a available"
+    message: "This is not a available",
   });
 });
 
 router.get("/:winnerField", (req, res, next) => {
   let winnerList = [];
+  let val = true;
+  if (req.params.winnerField === "false") val = false;
   db.collection("nominations")
-    .where("winner", "==", !Boolean(req.params.winnerField))
+    .where("winner", "==", val)
     .get()
-    .then(snapshot => {
+    .then((snapshot) => {
       if (snapshot.empty) {
         res.status(404).json({
           message: "Not a valid term",
-          field: req.params.winnerField
+          field: req.params.winnerField,
         });
         return;
       }
 
-      snapshot.forEach(doc => {
+      snapshot.forEach((doc) => {
         let info = doc.data();
         let data = {
           nominationId: doc.id,
-          info
+          info,
         };
         winnerList.push(data);
       });
       res.status(200).json({
-        winnerList
+        winnerList,
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log("Error getting documents", err);
     });
 });
@@ -57,7 +59,7 @@ router.get("/:winnerField", (req, res, next) => {
 // with the proper code like 502 or something
 router.delete("/:winnerField", (req, res, next) => {
   res.status(404).json({
-    message: "This is not a available"
+    message: "This is not a available",
   });
 });
 
